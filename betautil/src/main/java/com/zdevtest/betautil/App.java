9a5d1586c9f5bc2ba1385463a8b91508bd3d9531
srnpr <srnpr@srnprmacmini>
2014-02-16 16:31:16 +0800

    a

---
+++ betautil/src/main/java/com/zdevtest/betautil/App.java
@@ -0,0 +1,13 @@
+package com.zdevtest.betautil;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
